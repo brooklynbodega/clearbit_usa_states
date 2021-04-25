6e98537e01fc91d0cabf1ef1940994083363c34e
Raul Montero <raul.amontero@gmail.com>
2021-04-25 18:12:42 -0400

    added files

---
+++ states.js
@@ -0,0 +1,73 @@
+// Write a function that takes an object with a key of “stateCode”, 
+// a US state abbreviation and converts the abbreviation to the full State Name. 
+// In cases where an invalid value is used, prompt the user to enter a valid US State abbreviation. 
+// i.e. given company = {companyName: “Clearbit”, stateCode: “CA”} you’ll return “California”.
+
+
+// const stateCodes = require('./stateCodes.json');
+
+
+const stateCodes = {
+  "AL": "Alabama",
+  "AK": "Alaska",
+  "AZ": "Arizona",
+  "AR": "Arkansas",
+  "CA": "California",
+  "CO": "Colorado",
+  "CT": "Connecticut",
+  "DE": "Delaware",
+  "FL": "Florida",
+  "GA": "Georgia",
+  "HI": "Hawaii",
+  "ID": "Idaho",
+  "IL": "Illinois",
+  "IN": "Indiana",
+  "IA": "Iowa",
+  "KS": "Kansas",
+  "KY": "Kentucky",
+  "LA": "Louisiana",
+  "ME": "Maine",
+  "MD": "Maryland",
+  "MA": "Massachusetts",
+  "MI": "Michigan",
+  "MN": "Minnesota",
+  "MS": "Mississippi",
+  "MO": "Missouri",
+  "MT": "Montana",
+  "NE": "Nebraska",
+  "NV": "Nevada",
+  "NH": "New Hampshire",
+  "NJ": "New Jersey",
+  "NM": "New Mexico",
+  "NY": "New York",
+  "NC": "North Carolina",
+  "ND": "North Dakota",
+  "OH": "Ohio",
+  "OK": "Oklahoma",
+  "OR": "Oregon",
+  "PA": "Pennsylvania",
+  "RI": "Rhode Island",
+  "SC": "South Carolina",
+  "SD": "South Dakota",
+  "TN": "Tennessee",
+  "TX": "Texas",
+  "UT": "Utah",
+  "VT": "Vermont",
+  "VA": "Virginia",
+  "WA": "Washington",
+  "WV": "West Virginia",
+  "WI": "Wisconsin",
+  "WY": "Wyoming"
+};
+
+consoleLog(stateCodes);
+
+// Write the function that takes an user-created object with a key of “stateCode”, 
+// a US state abbreviation and converts the abbreviation to the value, the full State Name. 
+
+function stateName(input) {
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+  }
+}
